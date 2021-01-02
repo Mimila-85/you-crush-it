6965a0937a5d7facf23b920b30cf150c4fe1f587
Camila Alves Meyer <camila.alves85@gmail.com>
2021-01-03 01:35:57 +0800

    Created models/Exercise.js

---
+++ models/Exercise.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Exercise extends Model {}
+
+Exercise.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sub_category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "exercise",
+  }
+);
+
+module.exports = Exercise;
