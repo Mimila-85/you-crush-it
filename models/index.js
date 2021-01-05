de557cf9d895f168bdba07ee8c7bcd85cb56211d
Alfonso James <AlfonsoJames@users.noreply.github.com>
2021-01-05 17:20:24 -0500

    prepare for overwrite

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ const Routine = require("./Routine");
 const UserRoutine = require("./UserRoutine");
 
 // User belongToMany Routine (through UserRoutine)
-// Define the UserRoutine model as our through table. Sequilize creates a userId as foreign key.
+// Define the UserRoutine model as our through table. Sequelize creates a userId as foreign key.
 User.belongsToMany(Routine, { through: UserRoutine });
 
 // Routine belongToMany User (through UserRoutine).
-// Define the UserRoutine model as our through table. Sequilize creates a routineId as foreign key.
+// Define the UserRoutine model as our through table. Sequelize creates a routineId as foreign key.
 Routine.belongsToMany(User, { through: UserRoutine });
 
 // Package our models and export them as an object so we can import them together and use their proper names.
