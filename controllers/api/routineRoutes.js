2d1b5ed7e86c7933091c422b05589e91227fb409
Camila Alves Meyer <camila.alves85@gmail.com>
2021-01-08 10:34:47 +0800

    Fixed get routine on homeRoutes

--- controllers/api/routineRoutes.js
+++ controllers/api/routineRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { Routine } = require("../../models");
 const withAuth = require("../../utils/auth")
 
-router.post("/", async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const newRoutine = await Routine.create({
       ...req.body,
