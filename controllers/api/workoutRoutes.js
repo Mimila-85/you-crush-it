53df24b2a1d97f47dc6650c909d107bbbb8db37c
Camila Alves Meyer <camila.alves85@gmail.com>
2021-01-15 17:26:30 -0500

    Deleted unused code

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -15,53 +15,6 @@ router.post("/", withAuth, async (req, res) => {
   }
 });
 
-router.delete("/:id", withAuth, async (req, res) => {
-  try {
-    const workoutDelete = await Workout.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!workoutDelete) {
-      res.status(404).json({ message: "No workout found with this id!" });
-      return;
-    }
-
-    res.status(200).json(workoutDelete);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
-
-router.put("/:id", withAuth , 
-async (req, res) => {
-  try {
-    const updateWorkout = await Workout.update(
-      {
-      ...req.body,
-      },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    );
-
-    if (!updateWorkout) {
-      // Client error response 404 - Not found
-      res.status(404).json({ message: "No workout found with this id!" });
-      return;
-    }
-    // Success response 200 - OK
-    res.status(200).json(updateWorkout);
-  } catch (err) {
-    // Client error response 400 - Bad request
-    res.status(400).json(err.message);
-  }
-});
-
 router.get("/", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
