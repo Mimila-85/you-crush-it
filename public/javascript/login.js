a141431cc3e068d558b998af94a1d63e0200f139
Camila Alves Meyer <camila.alves85@gmail.com>
2021-01-15 17:06:47 -0500

    Fixed non compliance with ESLINT

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,22 +2,22 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email-input-login').value.trim();
-  const password = document.querySelector('#password-input-login').value.trim();
-console.log(email, password)
+  const email = document.querySelector("#email-input-login").value.trim();
+  const password = document.querySelector("#password-input-login").value.trim();
+  console.log(email, password);
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
-    const body = await response.json()
-    console.log(body)
+    const body = await response.json();
+    console.log(body);
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/dashboard');
+      document.location.replace("/dashboard");
     } else {
       alert(response.statusText);
     }
